92832277424bde9ac5c1d50e782e1ad69551656f
George Nikolopoulos <georgen@interactive-design.gr>
2023-06-20 11:41:44 +0300

    Update paymentMethods.js

--- .netlify/functions/paymentMethods.js
+++ .netlify/functions/paymentMethods.js
@@ -1,9 +1,12 @@
 exports.handler = async function(event, context, callback) {   
     // Get request's body
     const request = JSON.parse(event.body)
+    return {
+        statusCode: 200,
+        body: JSON.stringify(request),
+    }
 
     // Validate that the request is coming from Snipcart
-    /*
     const response = await fetch(`https://payment.snipcart.com/api/public/custom-payment-gateway/validate?publicToken=${request.PublicToken}`)
 
     // Return a 404 if the request is not from Snipcart
@@ -11,7 +14,6 @@ exports.handler = async function(event, context, callback) {
         statusCode: 404,
         body: ""
     }
-    */
 
     // Create a payment method list
     let paymentMethodList = [{
@@ -24,6 +26,6 @@ exports.handler = async function(event, context, callback) {
     // Return successful status code and available payment methods
     return {
         statusCode: 200,
-        body: JSON.stringify(paymentMethodList)
+        body: JSON.stringify(paymentMethodList),
     };
 }
