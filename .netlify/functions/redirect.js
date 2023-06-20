7bec66a1cdc30bbe3c3509706a6db69b79c089ca
George Nikolopoulos <georgen@interactive-design.gr>
2023-06-20 12:05:23 +0300

    Create redirect.js

---
+++ .netlify/functions/redirect.js
@@ -0,0 +1,16 @@
+exports.handler = async function(event, context, callback) {
+    if (event.httpMethod !== 'GET') {
+        return {
+            statusCode: 501,
+            body: JSON.stringify({ message: "Not Implemented" }),
+        }
+    }
+
+    // Return successful status code and available payment methods
+    return {
+        statusCode: 200,
+        headers: {
+          "Location": "https://www.interactive-design.gr",
+        },
+    };
+}
