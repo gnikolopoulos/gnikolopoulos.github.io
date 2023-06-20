d822dae150fd3ce86ae61beb2afce31f45acd329
George Nikolopoulos <georgen@interactive-design.gr>
2023-06-20 11:29:33 +0300

    Create paymentMethods.js

---
+++ .netlify/functions/paymentMethods.js
@@ -0,0 +1,27 @@
+exports.handler = async function(event, context, callback) {   
+    // Get request's body
+    const request = JSON.parse(event.body)
+
+    // Validate that the request is coming from Snipcart
+    const response = await fetch(`https://payment.snipcart.com/api/public/custom-payment-gateway/validate?publicToken=${request.PublicToken}`)
+
+    // Return a 404 if the request is not from Snipcart
+    if (!response.ok) return {
+        statusCode: 404,
+        body: ""
+    }
+
+    // Create a payment method list
+    let paymentMethodList = [{
+        id: 'vivawallet',
+        name: 'VivaWallet',
+        iconUrl: 'https://developer.vivawallet.com/images/new-payment-methods-logos/vivawallet.svg?width=50px',
+        checkoutUrl: 'google.com',
+    }]
+
+    // Return successful status code and available payment methods
+    return {
+        statusCode: 200,
+        body: JSON.stringify(paymentMethodList)
+    };
+}
